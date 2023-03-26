c7b07d8ddb241117c445a926364e1dbffe6ae7b0
Ivan Pivnev <pivnev-1998@mail.ru>
2023-03-26 16:04:18 +0300

    Написал вывод массива в терминал

--- LineArraySearch/Program.cs
+++ LineArraySearch/Program.cs
@@ -1,2 +1,13 @@
 ﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array2);
